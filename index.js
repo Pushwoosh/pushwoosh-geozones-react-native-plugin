29487941b9d90f3da17ea670f6775532a17e4f81
Sergey Polshcha <fectum@pushwoosh.com>
2019-02-13 16:20:52 +0700

    [feature][PUSH-17744] update location jar + dependency fix

--- index.js
+++ index.js
@@ -4,48 +4,17 @@ import { NativeModules } from 'react-native';
 
 const PushwooshGeozonesModule = NativeModules.PushwooshGeozones;
 
-//Class: PushNotification
-//Use `PushNotification` to register device for push notifications on Pushwoosh and customize notification appearance.
-//
-//Example:
-//(start code)
-//DeviceEventEmitter.addListener('pushOpened', (e: Event) => {
-//  console.warn("pushOpened: " + JSON.stringify(e));
-//  alert(JSON.stringify(e));
-//});
-//
-//const Pushwoosh = require('pushwoosh-react-native-plugin');
-//
-//Pushwoosh.init({ "pw_appid" : "XXXX-XXXX", "project_number" : "XXXXXXXXXXXXX" });
-//
-//Pushwoosh.register(
-//  (token) => {
-//    console.warn("Registered for pushes: " + token);
-//  },
-//  (error) => {
-//    console.warn("Failed to register: " + error);
-//  }
-//);
-//(end)
 class PushwooshGeozones {
 
 	//Function: startLocationTracking
-	//[android, ios, wp8, windows] Starts geolocation based push notifications. You need to configure Geozones in Pushwoosh Control panel.
-	//
-	//Parameters:
-	// "success" - success callback
-	// "fail" - error callback
+	//[android, ios] Starts geolocation based push notifications. You need to configure Geozones in Pushwoosh Control panel.
 	//
 	startLocationTracking() {
 		PushwooshGeozonesModule.startLocationTracking();
 	}
 
 	//Function: stopLocationTracking
-	//[android, ios, wp8, windows] Stops geolocation based push notifications
-	//
-	//Parameters:
-	// "success" - success callback
-	// "fail" - error callback
+	//[android, ios] Stops geolocation based push notifications
 	//
 	stopLocationTracking() {
 		PushwooshGeozonesModule.stopLocationTracking();

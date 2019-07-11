88506725b9672856fc1b2c36312ccfcc3b2d9ba5
Sergey Polshcha <fectum@pushwoosh.com>
2019-07-11 13:38:39 +0700

    [feature][PUSH-20910] react native 0.60 support

---
+++ react-native.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dependency: {
+    platforms: {
+    	ios: {
+        	project: "./src/ios/PushwooshGeozonesPlugin.xcodeproj"
+      	},
+      	android: {
+        	sourceDir: "./src/android"
+      	}
+    }
+  }
+};
